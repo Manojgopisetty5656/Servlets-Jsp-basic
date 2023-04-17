b25b965dd14f911ce3e63cbb9f5b6c52d88073d6
Manoj Gopisetty <85223503+Manojgopisetty5656@users.noreply.github.com>
2023-04-17 14:21:31 +0530

    Add files via upload

---
+++ DemoApp/src/main/java/com/manoj/SquareServlet.java
@@ -0,0 +1,39 @@
+package com.manoj;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+public class SquareServlet extends HttpServlet{
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		
+		//int k=Integer.parseInt(req.getParameter("k"));
+		HttpSession session=  req.getSession(); 
+		int k=(int)session.getAttribute("k");
+		
+		/*
+		 * usingcookies int k=0; Cookie cookies[]=req.getCookies(); for(Cookie c :
+		 * cookies) { if(c.getValue().equals("k")) k= Integer.parseInt(c.getValue());
+		 * 
+		 * }
+		 */
+		
+		 
+		k=k*k;
+		
+		PrintWriter out= res.getWriter();
+		out.println("Result is " + k);
+		
+		System.out.println("sq called");
+		
+	}
+}
